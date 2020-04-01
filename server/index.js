c705ad8114ef89a8ba4cdcedea4ca292d2e425dc
qurridutt@gmail.com <qurridutt@gmail.com>
2020-04-01 18:41:28 +0200

    Client: removed CRA boilerplate, created own setup. Backend: set up dummy server, route and socket connection

--- server/index.js
+++ server/index.js
@@ -1,1 +1,23 @@
-console.log("Hello!")
+import express from 'express';
+import socketio from 'socket.io';
+import http from 'http';
+
+const PORT = process.env.PORT || 5000;
+
+const router = require('./router')
+
+const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
+
+io.on('connection', (socket) => {
+    console.log("We have a new connection!")
+
+    socket.on('disconnect', () => {
+        console.log("User left.");
+    })
+});
+
+app.use(router);
+
+server.listen(PORT, () => console.log(`Server is up and running on ${PORT}`));
