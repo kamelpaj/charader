7e0ba26e16253423a5c56d8093d8285b11a569bb
qurridutt@gmail.com <qurridutt@gmail.com>
2020-04-01 22:38:02 +0200

    backend now sends which users are in a room

--- server/index.js
+++ server/index.js
@@ -20,6 +20,8 @@ io.on('connection', (socket) => {
         socket.broadcast.to(user.room).emit('message', { user: 'admin', text: `${user.name} has joined!` })
 
         socket.join(user.room);
+
+        io.to(user.room).emit('roomData', { room: user.room, users: getUsersInRoom(user.room)});
        
         callback();
     });
@@ -28,12 +30,18 @@ io.on('connection', (socket) => {
         const user = getUser(socket.id);
 
         io.to(user.room).emit('message', { user: user.name, text: message });
+        io.to(user.room).emit('message', { room: user.room, users: getUsersInRoom(user.room)});
 
         callback();
     });
 
     socket.on('disconnect', () => {
         console.log("User left.");
+        const user = removeUser(socket.id);
+
+        if (user) {
+            io.to(user.room).emit('message', { user: 'admin', text: `${user.name} has left the room` })
+        }
     })
 });
 
