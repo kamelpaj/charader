6ac4ab58ea901800006210f969e222c6ce700b8a
qurridutt@gmail.com <qurridutt@gmail.com>
2020-04-01 21:08:07 +0200

    socket functions for joining room and sending message

--- server/index.js
+++ server/index.js
@@ -1,24 +1,36 @@
 import express from 'express';
 import socketio from 'socket.io';
 import http from 'http';
+import { addUser, removeUser, getUser, GetUsersInRoom } from './users'
+import router from './router';
 
 const PORT = process.env.PORT || 5000;
-
-const router = require('./router')
-
 const app = express();
 const server = http.createServer(app);
 const io = socketio(server);
 
 io.on('connection', (socket) => {
     console.log("We have a new connection!");
-
     socket.on('join', ({ name, room }, callback) => {
-        console.log(name, room);
+        const { error, user } = addUser({ id: socket.id, name, room });
 
+        if (error) return callback(error);
 
+        socket.emit('message', { user: 'admin', text: `Welcome to the room, ${user.name}!` });
+        socket.broadcast.to(user.room).emit('message', { user: 'admin', text: `${user.name} has joined!` })
+
+        socket.join(user.room);
        
-    })
+        callback();
+    });
+
+    socket.on('sendMessage', (message, callback) => {
+        const user = getUser(socket.id);
+
+        io.to(user.room).emit('message', { user: user.name, text: message });
+
+        callback();
+    });
 
     socket.on('disconnect', () => {
         console.log("User left.");
